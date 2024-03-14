3413de442a761196bdf803bfa21f1603f8509b86
Dagmara Skulimowska <dagmaraskul@wp.pl>
2024-03-14 19:24:21 +0100

    added-basic-profile

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,2 +1,106 @@
-<template></template>
-<script></script>
+<template>
+  <div class="profile">
+    <div class="profile-info">
+      <div
+        class="profile-image"
+        :style="{
+          backgroundImage: user.image ? 'url(' + user.image + ')' : 'none',
+          backgroundColor: user.image ? 'transparent' : '#ccc',
+        }"
+      >
+        <div v-if="!user.image" class="placeholder"></div>
+      </div>
+      <div class="profile-description">
+        <h2 class="user-name">{{ user.name }}</h2>
+        <div class="description-container">
+          <p>{{ user.description }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="categories">
+      <a href="/bookslist">Przeczytane</a>
+      <a href="/bookslist">Aktualnie czytane</a>
+      <a href="/bookslist">Chcę przeczytać</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Nixanea",
+        description:
+          "Bookworms to super miejsce. Mam nadzieję, że będę się tutaj świetnie bawić :)",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10rem;
+}
+
+.profile-info {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 20px;
+}
+
+.profile-image {
+  margin-right: 20px;
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  background-color: #ccc;
+  background-size: cover;
+  background-position: center;
+}
+
+.profile-image img {
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+}
+
+.profile-description {
+  max-width: 40rem;
+}
+
+.user-name {
+  font-size: 2rem;
+  margin-top: 0;
+}
+
+.profile-description p {
+  margin-bottom: 0;
+}
+
+.categories {
+  display: flex;
+  margin-top: 100px;
+}
+
+.categories a {
+  text-decoration: none;
+  color: #333;
+  padding: 1rem 4rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 40px;
+}
+
+.categories a:last-child {
+  margin-right: 0;
+}
+
+.categories a:hover {
+  background-color: #f0f0f0;
+}
+</style>
